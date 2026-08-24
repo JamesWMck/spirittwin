5a8290e59e7d79c056e4be18c5195b4fb7cf4244
JamesWMck <1467979802@qq.com>
2026-08-24 18:03:27 +0800

    feat: add /thanks route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router'
 import Home from './pages/Home'
+import Thanks from './pages/Thanks'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/thanks" element={<Thanks />} />
     </Routes>
   )
 }
